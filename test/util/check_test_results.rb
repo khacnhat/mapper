0ed30e07c7e4d6ec4453d9a34b5da83381646ddd
Jon Jagger <jon@jaggersoft.com>
2019-01-04 11:09:29 +0000

    set test metrics

--- test/util/check_test_results.rb
+++ test/util/check_test_results.rb
@@ -92,11 +92,11 @@ table =
     [ 'failures',               failure_count,      '==',   0 ],
     [ 'errors',                 error_count,        '==',   0 ],
     [ 'skips',                  skip_count,         '==',   0 ],
-    [ 'duration(test)[s]',      test_duration,      '<=', 250 ],
+    [ 'duration(test)[s]',      test_duration,      '<=',   2 ],
     [ 'coverage(src)[%]',       src_coverage,       '==', 100 ],
     [ 'coverage(test)[%]',      test_coverage,      '==', 100 ],
-    [ 'lines(test)/lines(src)', f2(line_ratio),     '>=', 1.2 ],
-    [ 'hits(src)/hits(test)',   f2(hits_ratio),     '>=',   9 ],
+    [ 'lines(test)/lines(src)', f2(line_ratio),     '>=',   5 ],
+    [ 'hits(src)/hits(test)',   f2(hits_ratio),     '>=',  50 ],
   ]
 
 # - - - - - - - - - - - - - - - - - - - - - - -

9e963fdaf74408fee2b103545a981f3c4110bbf7
Jon Jagger <jon@jaggersoft.com>
2019-01-04 14:20:14 +0000

    added demo

--- client/src/demo.rb
+++ client/src/demo.rb
@@ -1,8 +1,69 @@
+require_relative 'ported_service'
 
 class Demo
 
   def call(_env)
-    # TODO
+    inner_call
+  rescue => error
+    [ 200, { 'Content-Type' => 'text/html' }, [ error.message ] ]
+  end
+
+  def inner_call
+    @html = ''
+    ready?
+    sha
+    ported?
+    ported_id
+    [ 200, { 'Content-Type' => 'text/html' }, [ @html ] ]
+  end
+
+  private
+
+  def ready?
+    result,duration = timed { ported.ready? }
+    @html += pre('ready?', duration, 'moccasin', result)
+  end
+
+  def sha
+    result,duration = timed { ported.sha }
+    @html += pre('sha', duration, 'moccasin', result)
+  end
+
+  def ported?
+    result,duration = timed { ported.ported?('33EBEA') }
+    @html += pre("ported?('33EBEA')", duration, 'moccasin', result)
+  end
+
+  def ported_id
+    result,duration = timed { ported.ported_id('33EBEAC') }
+    @html += pre("ported_id('33EBEAC')", duration, 'moccasin', result)
+  end
+
+  def ported
+    PortedService.new
+  end
+
+  def timed
+    started = Time.now
+    result = yield
+    finished = Time.now
+    duration = '%.3f' % (finished - started)
+    [result,duration]
+  end
+
+  def pre(name, duration, colour = 'white', result = nil)
+    border = 'border: 1px solid black;'
+    padding = 'padding: 5px;'
+    margin = 'margin-left: 30px; margin-right: 30px;'
+    background = "background: #{colour};"
+    whitespace = "white-space: pre-wrap;"
+    html = "<pre>(#{duration}s) /#{name}</pre>"
+    unless result.nil?
+      html += "<pre style='#{whitespace}#{margin}#{border}#{padding}#{background}'>" +
+              "#{JSON.pretty_unparse(result)}" +
+              '</pre>'
+    end
+    html
   end
 
 end
---
+++ sh/run_demo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+readonly SH_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+
+"${SH_DIR}/build_docker_images.sh"
+"${SH_DIR}/docker_containers_up.sh"
+
+echo "demo is on port=4548"

b4483bbabef5107ec561374cf5cff348a3e26fd2
Jon Jagger <jon@jaggersoft.com>
2019-01-05 14:07:14 +0000

    refactoring; remove duplication in demo code

--- client/src/demo.rb
+++ client/src/demo.rb
@@ -10,50 +10,31 @@ class Demo
 
   def inner_call
     @html = ''
-    ready?
-    sha
-    mapped?
-    mapped_id
+    pre('ready?') { mapper.ready? }
+    pre('sha') { mapper.sha }
+    pre("mapped?('33EBEA')") { mapper.mapped?('33EBEA') }
+    pre("mapped_id('33EBEAC')") { mapper.mapped_id('33EBEAC') }
     [ 200, { 'Content-Type' => 'text/html' }, [ @html ] ]
   end
 
   private
-
-  def ready?
-    @html += pre('ready?') { mapper.ready? }
-  end
-
-  def sha
-    @html += pre('sha') { mapper.sha }
-  end
-
-  def mapped?
-    @html += pre("mapped?('33EBEA')") { mapper.mapped?('33EBEA') }
-  end
-
-  def mapped_id
-    @html += pre("mapped_id('33EBEAC')") { mapper.mapped_id('33EBEAC') }
-  end
 
   def mapper
     MapperService.new
   end
 
   def pre(name, &block)
-    result,duration = timed { block.call }
     colour = 'moccasin'
     border = 'border: 1px solid black;'
     padding = 'padding: 5px;'
     margin = 'margin-left: 30px; margin-right: 30px;'
     background = "background: #{colour};"
     whitespace = "white-space: pre-wrap;"
-    html = "<pre>(#{duration}s) /#{name}</pre>"
-    unless result.nil?
-      html += "<pre style='#{whitespace}#{margin}#{border}#{padding}#{background}'>" +
-              "#{JSON.pretty_unparse(result)}" +
-              '</pre>'
-    end
-    html
+    result,duration = timed { block.call }
+    @html += "<pre>(#{duration}s) /#{name}</pre>"
+    @html += "<pre style='#{whitespace}#{margin}#{border}#{padding}#{background}'>" +
+            "#{JSON.pretty_unparse(result)}" +
+            '</pre>'
   end
 
   def timed
